1f67f93e6237da4dfbedf7c34db5b03e7c145e51
davidchen800 <davidchen800@gmail.com>
2014-03-08 20:09:07 -0800

    gotta get dem arrays

--- client/client.js
+++ client/client.js
@@ -5,3 +5,14 @@ Template.app.events({
  		console.log(interest);
  	}
 });
+Template.list.events({
+	'click button': function(evt){
+		var checked = []
+		$("input[name='interest']:checked").each(function ()
+		{
+		    checked.push($(this).val());
+		});
+		console.log(checked);
+		//Users.getUser(id).update(id, checked)
+	}
+})
